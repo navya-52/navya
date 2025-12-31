45bba31f87021b09ea8bf9c4a60058a8087b3ff3
navya-52 <navyasripuli7@gmail.com>
2025-12-31 15:17:26 +0530

    workdone

---
+++ navya 6552/chalenge1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>              
+
+int main() {                    
+    printf("Name: navya\n");    
+    printf("Age: 19\n");        
+    printf("Country: india\n"); 
+    return 0;                   
+}
